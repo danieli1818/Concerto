623558c6b4ecb8b17a4a71824fd12834de793a2b
BuildTools <unconfigured@null.spigotmc.org>
2020-11-13 18:27:44 +0200

    First Commit

---
+++ utils.py
@@ -0,0 +1,16 @@
+
+import re
+
+
+def youtube_url_validation(url):
+    youtube_regex = (
+        r'(https?://)?(www\.)?'
+        r'(youtube|youtu|youtube-nocookie)\.(com|be)/'
+        r'(watch\?v=|embed/|v/|.+\?v=)?([^&=%\?]{11})')
+
+    youtube_regex_match = re.match(youtube_regex, url)
+    if youtube_regex_match:
+        return youtube_regex_match
+
+    return youtube_regex_match
+
